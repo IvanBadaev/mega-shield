b09b54903879e17c615e7fbaced9476247224e09
Ivan <ivan.y.badaev@gmail.com>
2025-03-04 00:04:07 +0300

    removed hasVerifiedEmail in admin auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@mega-shield.ru') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@mega-shield.ru');
     }
 }
