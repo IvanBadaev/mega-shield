f79c4219172c0053989651bb802a163322345977
Ivan <ivan.y.badaev@gmail.com>
2025-03-04 00:23:30 +0300

    rename public_html to public

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            publicDirectory: 'public_html',
             input: ['resources/css/style.css', 'resources/js/App.jsx'],
             refresh: true,
         }),
