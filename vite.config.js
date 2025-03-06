7cf022fc6e5db58d036004bb0b0d5f9b6b8976ae
Ivan <ivan.y.badaev@gmail.com>
2025-03-06 20:04:57 +0300

    added base path to vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: 'http://mega-shield.ru/',
     plugins: [
         laravel({
             input: ['resources/css/style.css', 'resources/js/App.jsx'],
